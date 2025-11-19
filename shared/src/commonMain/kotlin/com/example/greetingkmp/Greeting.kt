66e6c8fc97802ea012df819407869eecbdb4f7f1
Debbie Mong'are <debbiearita96@gmail.com>
2025-11-19 11:47:45 +0300

    Update UI

--- shared/src/commonMain/kotlin/com/example/greetingkmp/Greeting.kt
+++ shared/src/commonMain/kotlin/com/example/greetingkmp/Greeting.kt
@@ -3,10 +3,10 @@ package com.example.greetingkmp
 import kotlin.random.Random
 
 class Greeting {
-    private val platform = getPlatform()
+    private val platform: Platform = getPlatform()
 
-    fun greet(): String {
-        val firstWord = if (Random.nextBoolean()) "Hi!" else "Hello"
-        return "$firstWord [$num] Guess what this is! > ${platform.name.reversed()}!"
+    fun greet(): List<String> = buildList {
+        add(if (Random.nextBoolean()) "Hi!" else "Hello")
+        add("Guess what this is! > ${platform.name.reversed()}")
     }
 }
