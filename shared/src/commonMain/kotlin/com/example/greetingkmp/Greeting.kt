fb48e214347d8ddf9dfa1ab1b674e1b9ca3d278f
Debbie Mong'are <debbiearita96@gmail.com>
2025-11-19 10:06:02 +0300

    Initialize GreetingKMP

---
+++ shared/src/commonMain/kotlin/com/example/greetingkmp/Greeting.kt
@@ -0,0 +1,12 @@
+package com.example.greetingkmp
+
+import kotlin.random.Random
+
+class Greeting {
+    private val platform = getPlatform()
+
+    fun greet(): String {
+        val firstWord = if (Random.nextBoolean()) "Hi!" else "Hello"
+        return "$firstWord [$num] Guess what this is! > ${platform.name.reversed()}!"
+    }
+}
